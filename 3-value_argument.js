1d979524acc2c9fba60eaa7cfa6b14eade5b3f88
Ann <annkarokiadmissions@gmail.com>
2025-07-10 23:38:34 +0300

    Add script that prints command-line arguments or 'no argument' if none is provided

---
+++ 3-value_argument.js
@@ -0,0 +1,15 @@
+const {argv} = require('node:process');
+
+const args = process.argv
+
+//no arguments = no arguments
+//no length, myVar
+
+if (args[2] === undefined) {
+	console.log("No argument")
+} else 
+	console.log(process.argv.slice(2))
+
+
+
+
