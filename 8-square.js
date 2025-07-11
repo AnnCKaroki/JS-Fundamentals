ff93e80521b953f59f9c710276082918f9b81aeb
Ann <annkarokiadmissions@gmail.com>
2025-07-11 18:07:50 +0300

    Add script that prints a sqaure

---
+++ 8-square.js
@@ -0,0 +1,17 @@
+//take size of square
+//missing size if not convertible to number
+//print using x
+
+const {argv} = require('node:process');
+
+const input = process.argv[2];
+
+const num = Number(input)
+
+if (Number.isNaN(num)) {
+        console.log("Missing size")
+} else if (num > 0) {
+     for (let i = 0; i < num; i++) {
+        console.log("x".repeat(num))
+     }
+}
