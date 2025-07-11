5104691eff3fb03ebf51d192d88e7c9d5638367f
Ann <annkarokiadmissions@gmail.com>
2025-07-11 14:44:11 +0300

    Fix output messages to 14 characters long

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -5,7 +5,7 @@ const input = process.argv[2];
 let num = Number(input);
 
 if (!Number.isNaN(num)) {
-	console.log(`My number is: ${num}`)
+	console.log(`My number: ${num}`)
 } 
 else {
         console.log("Not a number")
