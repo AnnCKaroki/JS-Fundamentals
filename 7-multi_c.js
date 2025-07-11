b996a0315f5607fa53feb7c71b223d9d62c775d1
Ann <annkarokiadmissions@gmail.com>
2025-07-11 17:16:20 +0300

    Add logic to handle negative arguments

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -8,10 +8,9 @@ const args = process.argv[2];
 
 const num = Number(args);
 
-if (!Number.isNaN(num)) {
-	for(let i=0; i<num; i++) {
-		console.log("C is fun")
-	};
-} else {
-        console.log("Missing number of occurrences")
-};
+if (Number.isNaN(num)) {
+	console.log("Missing number of occurrences")
+} else if (num > 0) {
+              for(let i=0; i < num; i++) {
+		      console.log("C is fun")
+}}
